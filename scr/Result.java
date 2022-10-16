abf034b535e4a56d85429f2ff913c51deb2904f9
radubaciuDell <radumbaciu@gmail.com>
2022-10-16 12:48:01 +0300

    tested some other ways

--- scr/Result.java
+++ scr/Result.java
@@ -5,38 +5,79 @@ public class Result {
     public static void getSubSequences(List<Integer> arr) {
         List<List<Integer>> sequence = new ArrayList<>();
 
-
+        /****************** With Do while */
 //        for (int i = 1; i <= arr.size(); i++) {
 //
 //            for (int j = 0; j < i; j++) {
 //                List<Integer> addSequence = new ArrayList<>();
 ////                for (int k =j ; k<i; k++){
 //                int k = j;
-//                   do{
-//                      addSequence.add(arr.get(k));
+//                do {
+//                    addSequence.add(arr.get(k));
 //
-//                      k++;
-//                  }while (k<i);
-//                System.out.println("Sequence [i"+i+"][j"+j+"] " + k + " is:" + addSequence + "\n");
-//                }
+//                    k++;
+//                } while (k < i);
+//                System.out.println("Sequence [i" + i + "][j" + j + "] " + k + " is:" + addSequence + "\n");
 //            }
 //        }
-        for (int i = 0; i < arr.size(); i++) {
+//    }
+        /****************** With Do while */
 
-            for (int j = i; j < arr.size(); j++) {
-                List<Integer> addSequence = new ArrayList<>();
-                addSequence.add(arr.get(j));
-                for (int k = 0; k < arr.size() - i; k++) {
-                    if (k!=j){addSequence.add(arr.get(k));}
-                    System.out.println("Sequence [i" + i + "][j" + j + "] " + k + " is:" + addSequence + "\n");
+        /****************** Starts with same number first */
 
+//
+//        for (int i = 0; i < arr.size(); i++) {
+//
+//            for (int j = i; j < arr.size(); j++) {
+//                List<Integer> addSequence = new ArrayList<>();
+//                addSequence.add(arr.get(j));
+//                for (int k = 0; k <arr.size() - i; k++) {
+//                    if (k!=j){addSequence.add(arr.get(k));}
+//                    System.out.println("Sequence [i" + i + "][j" + j + "] " + k + " is:" + addSequence + "\n");
+//
+//                }
+//
+//            }
+//        }
+//}
+        /************* Starts with same number first */
+
+
+        /************* Starts size 1 */
+//        for (int i = 0; i < arr.size(); i++) {
+//
+//            for (int j = 0; j < arr.size(); j++) {
+//                int k;
+//                List<Integer> addSequence = new ArrayList<>();
+//                addSequence.add(arr.get(j));
+//                for (k = j; k <= i; k++) {
+//                    if (k != j) {
+//                        addSequence.add(arr.get(k));
+//
+//                    }
+//
+//
+//                }
+//                System.out.println("Sequence [i" + i + "][j" + j + "] " + k + " is:" + addSequence + "\n");
+//            }
+//        }
+        /************* Starts size 1 */
+
+        /************* Starts size max */
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                for (int z = 0; z < 3; z++) {
+                    for (int k = 0; k < 3; k++) {
+                        if (x != y && y != z && z != x) {
+                            System.out.println(arr.get(x) + "" + arr.get(y) + "" + arr.get(z));
+                        }
+                    }
                 }
-
             }
+            /************* Starts size max */
         }
     }
 }
-
 
 //import java.util.ArrayList;
 //        import java.util.List;
